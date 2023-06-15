56ff62f4d7db805119e415f8b31d5b717f4f5d8a
MarcusVynicius1 <marcus.p@gsuite.iff.edu.br>
2023-06-15 12:58:32 -0300

    First Commit

---
+++ LojaDeAlimentos/src/main/java/com/example/LojaDeAlimentos/LojaDeAlimentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.LojaDeAlimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeAlimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeAlimentosApplication.class, args);
+	}
+
+}
---
+++ LojaDeAlimentos/src/test/java/com/example/LojaDeAlimentos/LojaDeAlimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LojaDeAlimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeAlimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
